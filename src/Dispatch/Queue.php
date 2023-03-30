001bc77d2238f247049581e39769dc1f5c363c6a
Glynn Quelch <glynn.quelch@gmail.com>
2023-03-30 11:20:15 +0100

    ..

--- src/Dispatch/Queue.php
+++ src/Dispatch/Queue.php
@@ -20,7 +20,7 @@ declare(strict_types=1);
  * @author Glynn Quelch <glynn.quelch@gmail.com>
  * @license http://www.opensource.org/licenses/mit-license.html  MIT License
  * @package PinkCrab\Queue
- * @since 0.1.0
+ * @since 2.0.0
  */
 
 namespace PinkCrab\Queue\Dispatch;
